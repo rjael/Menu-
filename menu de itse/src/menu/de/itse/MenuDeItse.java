5b8c2e4f44eeec0a529d9f5229af275c28dc37b8
unknown <you@example.com>
2012-11-05 14:42:06 -0600

    agrege el menu ayuda

--- menu de itse/src/menu/de/itse/MenuDeItse.java
+++ menu de itse/src/menu/de/itse/MenuDeItse.java
@@ -18,6 +18,11 @@ public class MenuDeItse
      *///
     public static void main(String[] args)
     {
-        
+         System.out.print("\nInstituto Tecnologico Superior de Escarcega \n Gemma Jael Romero Gonzalez ");
+                System.out.print("\nAyuda");
+                System.out.print("\nLa opcion 1: Invierte una palabra  ");
+                System.out.print("\nLa opcion 2: Imprime el largo de la cadena ");
+                System.out.print("\nLa opcion 3: Lee una cadena y 2 numeros");
+                System.out.print("\nLa opcion 4: Lee una cadena y el rango entre 2 numeros");
     }
 }
