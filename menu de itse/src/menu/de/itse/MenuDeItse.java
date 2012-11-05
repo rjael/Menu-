72d330025303d83b849eac806847e360086a0e81
unknown <you@example.com>
2012-11-05 14:25:52 -0600

    agrege proyecto

---
+++ menu de itse/src/menu/de/itse/MenuDeItse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu.de.itse;
+
+
+
+/**
+ *
+ * @author tania
+ */
+public class MenuDeItse
+{
+
+    /**
+     * @param args the command line arguments
+     *///
+    public static void main(String[] args)
+    {
+        
+    }
+}
